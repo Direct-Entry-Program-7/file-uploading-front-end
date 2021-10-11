95f913fe2d4df00d143c229ead5b3e727254d644
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-10-11 13:58:00 +0530

    Create Data URL from the selected image

--- ts/add-new-student.ts
+++ ts/add-new-student.ts
@@ -1,1 +1,19 @@
 import $ from 'jquery';
+
+const fr = new FileReader();
+
+/* Event listeners */
+
+$('#btn-browse').on('click', ()=> $('#file').trigger('click'));
+
+$('#file').on('input', ()=> {
+    const files = (document.getElementById('file') as HTMLInputElement).files;
+
+    if (files.length > 0){
+        fr.readAsDataURL(files[0]);
+    }
+});
+
+fr.addEventListener('load', ()=>{
+    $("#picture").attr('src', fr.result + "");
+});
